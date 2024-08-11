002a688700301ae9565ee558b8b5bd809272460e
Mark Diehr <mdiehr@gmail.com>
2024-08-11 15:22:20 -0400

    Move auth resolver to its own service

--- mpi/src/app/app.routes.ts
+++ mpi/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeviewComponent } from './homeview/homeview.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
-import { MovieService } from './services/movies.api';
+import { AuthResolverService } from './services/auth-resolver.service';
 
 export const routes: Routes = [
-  {path: '', component: HomeviewComponent, title: "Search - Moving Picture Interface"},
-  {path: 'movie/:id', component: MovieDetailsComponent,
+  {path: '', component: HomeviewComponent, title: "Search - Moving Picture Interface",
     resolve: {
-      isLoggedIn: MovieService
+      isLoggedIn: AuthResolverService
+    }
+  },
+  {path: 'movie/:id', component: MovieDetailsComponent, title: "Movie - Moving Picture Interface",
+    resolve: {
+      isLoggedIn: AuthResolverService
     }
   },
 ];
