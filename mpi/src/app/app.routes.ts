adaa00af6fd36742d6fb053b4c3dd448512adb78
Mark Diehr <mdiehr@gmail.com>
2024-08-11 15:16:56 -0400

    Resolve auth status before displaying movie details

--- mpi/src/app/app.routes.ts
+++ mpi/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeviewComponent } from './homeview/homeview.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieService } from './services/movies.api';
 
 export const routes: Routes = [
   {path: '', component: HomeviewComponent, title: "Search - Moving Picture Interface"},
-  {path: 'movie/:id', component: MovieDetailsComponent},
+  {path: 'movie/:id', component: MovieDetailsComponent,
+    resolve: {
+      isLoggedIn: MovieService
+    }
+  },
 ];
