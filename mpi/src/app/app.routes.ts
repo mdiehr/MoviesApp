55d97c22f1d089a9b98d09866da3ee5b9cfdbdc3
Mark Diehr <mdiehr@gmail.com>
2024-08-12 14:35:43 -0400

    Create not found page

--- mpi/src/app/app.routes.ts
+++ mpi/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { AuthResolverService } from './services/auth-resolver.service';
 import { MovieTitleResolver } from './services/movie-title-resolver.service';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent,
@@ -13,4 +14,5 @@ export const routes: Routes = [
     title: MovieTitleResolver,
     resolve: { isLoggedIn: AuthResolverService }
   },
+  { path: '**', component: NotFoundPageComponent}
 ];
