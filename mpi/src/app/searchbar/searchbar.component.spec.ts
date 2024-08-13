ee619957e7b78ddbd160194b77635c174eb583f3
Mark Diehr <mdiehr@gmail.com>
2024-08-13 15:55:20 -0400

    Linting

--- mpi/src/app/searchbar/searchbar.component.spec.ts
+++ mpi/src/app/searchbar/searchbar.component.spec.ts
@@ -5,7 +5,6 @@ import { provideMockStore } from '@ngrx/store/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
 
 describe('SearchbarComponent', () => {
   let component: SearchbarComponent;
