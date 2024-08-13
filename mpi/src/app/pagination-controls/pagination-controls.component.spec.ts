8178a21077603c24d6cedb26084443f62973428f
Mark Diehr <mdiehr@gmail.com>
2024-08-12 20:00:42 -0400

    Basic default tests now pass

--- mpi/src/app/pagination-controls/pagination-controls.component.spec.ts
+++ mpi/src/app/pagination-controls/pagination-controls.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaginationControlsComponent } from './pagination-controls.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('PaginationControlsComponent', () => {
   let component: PaginationControlsComponent;
@@ -8,7 +10,15 @@ describe('PaginationControlsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PaginationControlsComponent]
+      imports: [PaginationControlsComponent],
+      providers: [
+        { provide: ActivatedRoute,  
+          useValue: 
+          {
+            queryParamMap: of({page: 1})
+          }
+        }
+      ]
     })
     .compileComponents();
 
